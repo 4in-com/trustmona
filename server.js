2e2679e961b790431f18c23e003d4356bf9ce57b
russel muyembe <russelmuyembe9@gmail.com>
2026-01-06 15:42:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ Return ONLY valid JSON:
 {
   "risk_level": "low | medium | high",
   "risk_score": 0-100,
-  "reasons": ["short reason 1", "short reason 2","long explaination on what to do to avoid sacm from this,it need to start with What To Do:here help explanation"]
+  "reasons": ["short reason 1", "short reason 2","short example from a person that had been scamed some way and his/her country name","long explaination on what to do to avoid sacm from this,it need to start with What To Do:here help explanation"]
 }
 
 Message:
